389348981d1b6e27efeca0d1cb30a4ece3ef0c18
mervemetinoglu <mervemetinogluu@gmail.com>
2024-03-01 19:12:42 +0300

    create search and favorites list view controllers

---
+++ GHFollowers/FavoritesListVC.swift
@@ -0,0 +1,15 @@
+//
+//  FavoritesListVC.swift
+//  GHFollowers
+//
+//  Created by mervemetinoglu on 1.03.2024.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+}
---
+++ GHFollowers/SearchVC.swift
@@ -0,0 +1,35 @@
+//
+//  SearchVC.swift
+//  GHFollowers
+//
+//  Created by mervemetinoglu on 1.03.2024.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+    let logoImageView = UIImageView()
+    let usernameTextField = GFTextField()
+    let callToActionButton = GFButton(backgroundColor: .systemGreen, title: "Get Followers")
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
+    }
+
+    func configureLogoImageView() {
+        view.addSubview(logoImageView)
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+        logoImageView.image = UIImage(named: "gh-logo")!
+
+        NSLayoutConstraint.activate([
+            logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 80),
+            logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+    }
+}
