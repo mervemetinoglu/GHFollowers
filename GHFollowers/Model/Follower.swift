e55ce6ac1804d08128751de64895ea531f1d3eb0
mervemetinoglu <mervemetinogluu@gmail.com>
2024-03-04 17:32:36 +0300

    create follower & user models

---
+++ GHFollowers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GHFollowers
+//
+//  Created by mervemetinoglu on 4.03.2024.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
