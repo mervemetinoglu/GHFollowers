914c00b6796dc6b4f5102b073dfb3560be2cca14
mervemetinoglu <mervemetinogluu@gmail.com>
2024-03-04 15:20:23 +0300

    create custom alert view

--- GHFollowers/SearchVC.swift
+++ GHFollowers/SearchVC.swift
@@ -37,7 +37,9 @@ class SearchVC: UIViewController {
 
     @objc func pushFollowerListVC() {
         guard isUsernameEntered else {
-            print("No username")
+            presentGFAlertOnMainThread(title: "Empty Username",
+                                       message: "Please enter a username. We need to know who to look for.",
+                                       buttonTitle: "OK")
             return
         }
 
