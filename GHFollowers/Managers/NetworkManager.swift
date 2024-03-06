6c0667e6da8c729897be4c591eaf06534ab0ab31
mervemetinoglu <mervemetinogluu@gmail.com>
2024-03-06 18:05:19 +0300

    add cache for images

--- GHFollowers/Managers/NetworkManager.swift
+++ GHFollowers/Managers/NetworkManager.swift
@@ -5,11 +5,12 @@
 //  Created by mervemetinoglu on 4.03.2024.
 //
 
-import Foundation
+import UIKit
 
 class NetworkManager {
     static let shared = NetworkManager()
-    let baseURL = "https://api.github.com/users/"
+    private let baseURL = "https://api.github.com/users/"
+    let cache = NSCache<NSString, UIImage>()
 
     private init() {}
 
