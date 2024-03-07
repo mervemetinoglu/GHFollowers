4be7f216d147f373caa31e44523996236d06e1ac
mervemetinoglu <mervemetinogluu@gmail.com>
2024-03-07 16:34:38 +0300

    create sfsymbols constants

---
+++ GHFollowers/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  GHFollowers
+//
+//  Created by mervemetinoglu on 7.03.2024.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
