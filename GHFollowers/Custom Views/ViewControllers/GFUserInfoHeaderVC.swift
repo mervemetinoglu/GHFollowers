985cf2e74125a157d434891df05d27536f9beff1
mervemetinoglu <mervemetinogluu@gmail.com>
2024-03-08 23:21:02 +0300

    move download image func to network manager

--- GHFollowers/Custom Views/ViewControllers/GFUserInfoHeaderVC.swift
+++ GHFollowers/Custom Views/ViewControllers/GFUserInfoHeaderVC.swift
@@ -34,8 +34,28 @@ class GFUserInfoHeaderVC: UIViewController {
         configureUIElements()
     }
 
+    func addSubviews() {
+        view.addSubview(avatarImageView)
+        view.addSubview(usernameLabel)
+        view.addSubview(nameLabel)
+        view.addSubview(locationImageView)
+        view.addSubview(locationLabel)
+        view.addSubview(bioLabel)
+    }
+
+    func downloadAvatarImage() {
+        NetworkManager.shared.downloadImage(from: user.avatarUrl) { [weak self] image in
+            guard let self = self else { return }
+
+            DispatchQueue.main.async {
+                self.avatarImageView.image = image
+            }
+        }
+    }
+
     func configureUIElements() {
-        avatarImageView.downloadImage(from: user.avatarUrl)
+        downloadAvatarImage()
+
         usernameLabel.text = user.login
         nameLabel.text = user.name ?? ""
         locationLabel.text = user.location ?? "No Location"
@@ -45,15 +65,6 @@ class GFUserInfoHeaderVC: UIViewController {
 
         locationImageView.image = UIImage(systemName: SFSymbols.location)
         locationImageView.tintColor = .secondaryLabel
-    }
-
-    func addSubviews() {
-        view.addSubview(avatarImageView)
-        view.addSubview(usernameLabel)
-        view.addSubview(nameLabel)
-        view.addSubview(locationImageView)
-        view.addSubview(locationLabel)
-        view.addSubview(bioLabel)
     }
 
     func layoutUI() {
