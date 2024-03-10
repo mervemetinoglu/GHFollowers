ee298bcb60606f4b6890ead5f1c1dd70b1480000
mervemetinoglu <mervemetinogluu@gmail.com>
2024-03-10 22:58:45 +0300

    refactor: remove tabbar background

--- GHFollowers/Custom Views/TabBarControllers/GFTabBarController.swift
+++ GHFollowers/Custom Views/TabBarControllers/GFTabBarController.swift
@@ -10,11 +10,6 @@ import UIKit
 class GFTabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        let appearance = UITabBarAppearance()
-        appearance.configureWithOpaqueBackground()
-
-        UITabBar.appearance().standardAppearance = appearance
-        UITabBar.appearance().scrollEdgeAppearance = appearance
         UITabBar.appearance().tintColor = .systemGreen
 
         viewControllers = [createSearchNC(), createFavoritesNC()]
