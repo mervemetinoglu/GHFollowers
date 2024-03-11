4c4cbb1ec913a9e246a21e67573c350b22349f0f
mervemetinoglu <mervemetinogluu@gmail.com>
2024-03-11 23:25:43 +0300

    refactor: remove old UIKit codes

--- GHFollowers/Custom Views/Cells/FollowerCell/FollowerCell.swift
+++ GHFollowers/Custom Views/Cells/FollowerCell/FollowerCell.swift
@@ -11,12 +11,8 @@ import UIKit
 class FollowerCell: UICollectionViewCell {
     static let reuseID = "FollowerCell"
 
-    let avatarImageView = GFAvatarImageView(frame: .zero)
-    let usernameLabel = GFTitleLabel(textAlignment: .center, fontSize: 16)
-
     override init(frame: CGRect) {
         super.init(frame: frame)
-        configure()
     }
 
     @available(*, unavailable)
@@ -25,31 +21,8 @@ class FollowerCell: UICollectionViewCell {
     }
 
     func set(_ follower: Follower) {
-        if #available(iOS 16.0, *) {
-            contentConfiguration = UIHostingConfiguration {
-                FollowerView(follower: follower)
-            }
-        } else {
-            usernameLabel.text = follower.login
-            avatarImageView.downloadImage(fromURL: follower.avatarUrl)
+        contentConfiguration = UIHostingConfiguration {
+            FollowerView(follower: follower)
         }
-    }
-
-    private func configure() {
-        addSubviews(avatarImageView, usernameLabel)
-
-        let padding: CGFloat = 8
-
-        NSLayoutConstraint.activate([
-            avatarImageView.topAnchor.constraint(equalTo: topAnchor, constant: padding),
-            avatarImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: padding),
-            avatarImageView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -padding),
-            avatarImageView.heightAnchor.constraint(equalTo: avatarImageView.widthAnchor),
-
-            usernameLabel.topAnchor.constraint(equalTo: avatarImageView.bottomAnchor, constant: 12),
-            usernameLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: padding),
-            usernameLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -padding),
-            usernameLabel.heightAnchor.constraint(equalToConstant: 20)
-        ])
     }
 }
